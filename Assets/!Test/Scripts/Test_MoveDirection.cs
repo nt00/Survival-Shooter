9403f5e1b960b60b73905c05a40ffc9a6f5a2198
NTam <nichole.cy.tam@gmail.com>
2017-12-08 00:50:33 +1100

    Added Testing for Jumping and GUI

--- Assets/!Test/Scripts/Test_MoveDirection.cs
+++ Assets/!Test/Scripts/Test_MoveDirection.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test_MoveDirection : MonoBehaviour {
+public class Test_MoveDirection : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
---
+++ Assets/TestingSurvivalShooter/Scripts/Test_Jump.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using GGL;
+[RequireComponent(typeof(PlayerUtility))]
+public class Test_Jump : Test
+{
+    [Header("Test Parameters")]
+    public float minHeight = 1f;
+
+    private PlayerUtility player;
+    private float originalY;
+    private float jumpApex;
+
+    void Start()
+    {
+        player = GetComponent<PlayerUtility>();
+        originalY = transform.position.y;
+    }
+    public override void Simulate()
+    {
+        // Simulates the jump mechanic on the player
+        player.Jump();
+    }
+
+    public override void Check()
+    {
+        // Get current player position on Y
+        float playerY = player.transform.position.y;
+        float height = playerY - originalY;
+        // Get the apex of the player's jump
+        if (height > jumpApex)
+            jumpApex = height;
+
+        // Check if the current Y is higher than starting position
+        if (jumpApex > minHeight)
+        {
+            // The test has succeeded
+            IntegrationTest.Pass(gameObject);
+        }
+    }
+    public override void Debug()
+    {
+        GizmosGL.color = Color.red;
+        Vector3 originalPos = new Vector3(0, originalY, 0);
+        Vector3 playerPos = transform.position;
+        // Draw the min height the player needs to jump to
+        GizmosGL.AddLine(originalPos, originalPos + Vector3.up * minHeight);
+
+        // Draw the player's current height
+        GizmosGL.AddLine(originalPos, playerPos, 0.35f, 0.35f);
+    }
+}
